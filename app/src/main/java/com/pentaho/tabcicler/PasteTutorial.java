266de1068dbee70e593fa4c4cbbcf918dddd588f
Gonçalo Neto <gmneto@pentaho.com>
2017-08-11 14:47:05 +0100

    Fix Tut Page

--- app/src/main/java/com/pentaho/tabcicler/PasteTutorial.java
+++ app/src/main/java/com/pentaho/tabcicler/PasteTutorial.java
@@ -3,6 +3,8 @@ package com.pentaho.tabcicler;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 /**
  * Created by gmneto on 11/08/2017.
@@ -14,5 +16,13 @@ public class PasteTutorial extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paste_tutorial);
 
+        FloatingActionButton close = (FloatingActionButton) findViewById(R.id.xButton);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
